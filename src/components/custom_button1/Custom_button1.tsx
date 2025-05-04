1d132cf84443143a31848646e6095bbf84f6cc04
sekeerVs <jomartolentino2002@gmail.com>
2025-05-04 09:29:31 +0800

    Refactor Bootstrap styles and update theme colors; add Custom_button1 component with styles; enhance Home layout with search functionality and outfit recommendation button

---
+++ src/components/custom_button1/Custom_button1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Custom_button1() {
+  return (
+    <div>Custom_button1</div>
+  )
+}
+
+export default Custom_button1
--- src/layouts/home/Home.tsx
+++ src/layouts/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Image, Button, Form } from 'react-bootstrap';
 import store_1 from '../../assets/images/store_1.jpg';
+import { Stars } from 'react-bootstrap-icons';
 import './home.css';
 
 function Home() {
@@ -12,19 +13,36 @@ function Home() {
           alt="Store image"
           className="w-100 object-fit-cover"
         />
-        <div className="position-absolute top-50 start-50 translate-middle bg-black bg-opacity-25 p-3 rounded-1">
-          <h2 className="fw-bold text-white text-center">Find your perfect outfit</h2>
-          <Form className="search-style d-flex gap-3">
-            <Form.Group controlId="formBasicEmail" className="mb-0 flex-grow-1">
-              <Form.Control type="text" placeholder="Look for dress color, type, or name" size="lg" />
-            </Form.Group>
 
-            <Button variant="primary" size="lg" type="submit">
-              Search
+        <div className="w-auto h-auto position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center">
+          <div className="bg-black bg-opacity-25 p-3 rounded-1 text-center">
+            <h2 className="fw-bold text-white">Find your perfect outfit</h2>
+            <Form className="search-style d-flex gap-3">
+              <Form.Group controlId="formBasicEmail" className="mb-0 flex-grow-1">
+                <Form.Control
+                  type="text"
+                  placeholder="Look for dress color, type, or name"
+                  size="lg"
+                />
+              </Form.Group>
+              <Button variant="primary" size="lg" type="submit">
+                Search
+              </Button>
+            </Form>
+          </div>
+
+          {/* Button spacing */}
+          <div className="w-auto h-auto d-flex align-items-center mt-4 rounded-1 overflow-hidden border-black"> 
+            <Button
+              className=" bg-secondary text-white fw-semibold"
+            >
+              <Stars size={20} />
+              Outfit Recommendation
             </Button>
-          </Form>
+          </div>
         </div>
       </div>
+
     </Container>
   );
 }
